46d2b877bc045df3938df1973c8a29049377ccf5
DESKTOP-41KE6EB\ram01 <abrawat01@gmail.com>
2025-02-06 21:19:27 +0530

    new update

--- js/mail.js
+++ js/mail.js
@@ -1,28 +1,18 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
   const form = event.target;
-  const formMessage = document.getElementById('form-message');
 
-  formMessage.style.display = 'block';
-  formMessage.style.color = 'blue';
-  formMessage.style.fontWeight = '300';
-  formMessage.style.textShadow = '0px 0px 6px rgb(0, 191, 255)';
-  formMessage.textContent = 'Sending your message...';
-
+  alert('Sending your message...');
 
   fetch(form.action, {
       method: form.method,
       body: new FormData(form),
       mode: 'no-cors'
   }).then(() => {
-      formMessage.style.display = 'block';
-      formMessage.style.color = 'green';
-      formMessage.style.textShadow = '0px 0px 6px rgb(0, 255, 242)';
-      formMessage.textContent = 'Thank you! I will get back to you soon...';
+      alert('Thank you! I will get back to you soon...');
       form.reset();
+      window.location.href = 'https://mr-rabot.github.io/mysite/'; // Redirect to homepage
   }).catch(() => {
-      formMessage.style.display = 'block';
-      formMessage.style.color = 'red';
-      formMessage.textContent = 'Oops! There was a problem submitting your form.';
+      alert('Oops! There was a problem submitting your form.');
   });
 });
