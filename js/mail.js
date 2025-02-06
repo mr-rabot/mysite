5d23d88199bc1a237120f9ac3e3303916c901391
DESKTOP-41KE6EB\ram01 <abrawat01@gmail.com>
2025-02-06 20:00:16 +0530

    new update

--- js/mail.js
+++ js/mail.js
@@ -8,8 +8,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     Email.send({
         Host: "smtp.gmail.com",
         Username: "rjii.soft1122@gmail.com",  // Your Gmail address
-        Password: "moedcbygeeznolon",  // App Password or actual password (if less secure apps enabled)
-        To: 'rjii.soft1122@gmail.com',  // Replace with your receiving email
+        Password: "gtiasiztzslgdgqr",  // App Password or actual password (if less secure apps enabled)
+        To: 'abrawat01@gmail.com',  // Replace with your receiving email
         From: email,  // The sender's email from the form
         Subject: `New Contact Form Submission from ${name}`,
         Body: `Name: ${name}<br>Email: ${email}<br>Message: ${message}`
