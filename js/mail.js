db1d832447b022277f38e5705c4f23a48da0062b
DESKTOP-41KE6EB\ram01 <abrawat01@gmail.com>
2025-02-06 20:38:34 +0530

    new update

--- js/mail.js
+++ js/mail.js
@@ -3,6 +3,13 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   const form = event.target;
   const formMessage = document.getElementById('form-message');
 
+  // Show loading message
+  formMessage.style.display = 'block';
+  formMessage.style.color = 'blue';
+  formMessage.style.fontWeight = '300';
+  formMessage.style.textShadow = '0px 0px 6px rgb(0, 191, 255)';
+  formMessage.textContent = 'Sending your message...';
+
   fetch(form.action, {
     method: form.method,
     body: new FormData(form),
@@ -11,21 +18,15 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     }
   }).then(response => {
     if (response.ok) {
-      formMessage.style.display = 'block';
       formMessage.style.color = 'green';
-      formMessage.style.fontFamily= '"Courier New", Courier, monospace';
-      formMessage.style.fontWeight= '300';
-      formMessage.style.textShadow= '0px 0px 6px rgb(0, 255, 242)';
+      formMessage.style.textShadow = '0px 0px 6px rgb(0, 255, 242)';
       formMessage.textContent = 'Thank you! I will get back to you soon...';
       form.reset();
     } else {
       throw new Error('Network response was not ok.');
     }
   }).catch(error => {
-    formMessage.style.display = 'block';
     formMessage.style.color = 'red';
-    formMessage.style.fontFamily= '"Courier New", Courier, monospace';
-    formMessage.style.fontWeight= '300';
     formMessage.textContent = 'Oops! There was a problem submitting your form.';
   });
 });
